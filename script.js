d087ccf5c915d7df18dcebe3d2bd2d945d1ccbc9
Alessandro Santos <alesaintts5@gmail.com>
2025-01-21 12:57:36 -0300

    Modificações feitas

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
+    const emailPattern =  /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
+
+    const userEmailInput = document.getElementById('useremail');
+    const userPasswordInput = document.getElementById('password');
+
     const useremail = document.getElementById('useremail').value;
     const password = document.getElementById('password').value;
-    const messageError = document.getElementById('messageerror');
-    const message = document.getElementById('message');
 
-    const emailPattern =  /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const errorMessage = document.getElementById('errormessage');
+    const rightMessage = document.getElementById('rightmessage');
 
     if (useremail === '' && password === '') {
-        messageError.style.color = 'red';
-        messageError.textContent = "Preencha os campos acima!";
-    } else if (!emailPattern.test(useremail)) {
-        messageError.style.color = 'red';
-        messageError.textContent = 'Por favor, insira um e-mail valida!';
+        errorMessage.style.color = 'red';
+        errorMessage.textContent = "Preencha os campos acima!";
+    } else if (!emailPattern.test(useremail) && !passwordPattern.test(password)) {
+        userEmailInput.style.borderColor = 'red';
+        userPasswordInput.style.borderColor = 'red';
+        errorMessage.style.color = 'red';
+        errorMessage.textContent = 'Insira e-mail e uma senha valida!';
     } else if (password.length < 6) {
-        messageError.style.color = 'red';
-        messageError.textContent = 'A senha deve ter pelo menos 6 caracteres!';
+        userPasswordInput.style.borderColor = '2px solid red';
+        errorMessage.style.color = 'red';
+        errorMessage.textContent = 'A senha deve ter pelo menos 6 caracteres!';
     } else {
-        messageError.style.color = 'green';
-        messageError.textContent = 'Login realizado com sucesso!';
-        
+        userEmailInput.style.borderColor = 'green';
+        userPasswordInput.style.borderColor = 'green';
+
         setTimeout(() => {
-            message.style.display = 'block';
-            message.style.color = 'white';
-            message.textContent = 'Enviado com sucesso!';
+            rightMessage.style.display = 'block';
+            rightMessage.style.color = 'white';
+            rightMessage.textContent = 'Enviado com sucesso!';
+
+            console.table(useremail, password);
         }, 2000);
     }
 });
